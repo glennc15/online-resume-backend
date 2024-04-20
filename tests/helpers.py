9986287d042169bfa24942df168be50776a65727
Glenn <gcrosby15@gmail.com>
2024-04-19 20:33:06 -0500

    fix path to the new terraform directory in get_terraform_outputs(). Can now run the integration tests again

--- tests/helpers.py
+++ tests/helpers.py
@@ -11,7 +11,7 @@ def get_dynamodb_json():
 
 
 def get_terraform_outputs():
-    terraform_outputs = subprocess.run(['terraform output'], stdout=subprocess.PIPE, shell=True)
+    terraform_outputs = subprocess.run(['cd terraform && terraform output'], stdout=subprocess.PIPE, shell=True)
     terraform_str = terraform_outputs.stdout.decode('utf-8')
     terraform_parts = [x.split(' = ') for x in terraform_str.split('\n')]
     terraform_outputs = dict([(x[0], x[1][1:-1]) for x in terraform_parts if len(x) > 1])
